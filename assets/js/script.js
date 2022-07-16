36e319fc983acd8fdf6dfcbba4089f33d308e904
pabco84 <pabco84@gmail.com>
2022-07-16 17:26:16 -0400

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,31 +2,62 @@
 const Listadetareas = document.querySelector("#Tareastotales")
 const tareainput = document.querySelector("#Tareas")
 const btnAgregar = document.querySelector("#agregarTarea")
+var contadortareas = 4;
+let tot = document.querySelector("#totales")
+let realizadas =document.querySelector("#realizadas")
 
 const tareas = [
     {
-        ID: 16456345,
+        ID: 1,
         Tarea: "Apagar el despetador"
     },
     {
-        ID: 78456385,
+        ID: 2,
         Tarea: "Ducharse y tomar desayuno"
     },
     {
-        ID: 31695634,
-        Tarea: "Preparar se para la aventura"
+        ID: 3,
+        Tarea: "Prepararse para la aventura"
     },
 ]
-btnAgregar.addEventListener("click", () => {
-console.log(tareainput)
-const nombretarea = {ID: Date.now(), Tarea:tareainput.value}
-console.log(nombretarea)
-tareas.push(nombretarea)
-tareainput.value = ""
 
+/* Agregador de tareas */
+btnAgregar.addEventListener("click", () => {
+    if (tareainput.value === ""){
+    alert ("Ingrese una tarea");}
+    else {
+const nombretarea = {ID: contadortareas, Tarea:tareainput.value}
+contadortareas = contadortareas + 1;
+tareas.push(nombretarea);
+tareainput.value = "";
+
+
+
+/* Esritor en la pantalla de las nuevas tareas */
 let html = ""
 for (let pega of tareas) {
-html += `<li style="list-style: none;">${pega.ID} ${pega.Tarea} <button onclick="borrar(${pega.id})"> x </button></li>`;
+html += `<li style="list-style: none;">${pega.ID} ${pega.Tarea} <button onclick="borrar(${pega.ID})"> x </button> <input type="checkbox"></li>`;
 }
 Listadetareas.innerHTML = html;
-})
+tot.innerHTML="Total:" + tareas.length;
+
+}})
+
+/* eliminador de tareas */
+function borrar(ID){
+const nombretarea = tareas.findIndex((ele) => ele.ID == ID)
+contadortareas = contadortareas - 1;
+tareas.splice(nombretarea, 1);
+tareainput.value = "";
+
+
+
+/* Esritor en la pantalla de las nuevas tareas */
+let html = ""
+for (let pega of tareas) {
+html += `<li style="list-style: none;">${pega.ID} ${pega.Tarea} <button onclick="borrar(${pega.ID})"> x </button> <input type="checkbox"></li>`;
+}
+Listadetareas.innerHTML = html;
+tot.innerHTML="Total:" + tareas.length;
+
+}
